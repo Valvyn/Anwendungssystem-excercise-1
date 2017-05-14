6ed5bad3b50fb3d0fa840ef3c6e96da334d191fa
flattermann <friedrich.lattermann@kaeuferportal.de>
2017-05-14 14:10:07 +0200

    Initial commit

---
+++ script1.js
@@ -0,0 +1,65 @@
+$( document ).ready(function(){
+
+ 
+});
+
+function zufallsZahl1() {
+    var elem1;
+    var elem2;
+    var min = 220;
+    var max = 240;
+    var min2 = 0;
+    var max2 = 7;
+ 
+    
+    elem2 = (Math.random() * (max - min + 1)) + min;
+    elem1 = Runden2Dezimal(elem2)
+
+    if(elem1 != ""){
+        elem4=(Math.random() * (max2 - min2 + 1)) + min2;
+        elem3 = Runden2Dezimal(elem4)
+        document.getElementById("ausgabe2").innerHTML = elem3;
+        if(elem3 > 5){
+        alert("Warnung, Stromstärke überprüfen!");
+    }
+    }
+    
+
+    document.getElementById("ausgabe1").innerHTML = elem1;
+    
+}
+
+
+function Runden2Dezimal(elem1) { 
+    Ergebnis = Math.round(elem1 * 10) / 10 ; 
+    return Ergebnis; }
+
+
+
+
+var elem = document.getElementById('los');
+elem.addEventListener('click', chkFormular);
+
+
+if (document.Formular.Verbrauch.value != "" && document.Formular.Name != "") { 
+        elemUhr = new Date();
+		//alert("Test!");
+        document.getElementById("ausgabe3").innerHTML = new Date;
+    
+    }
+
+function chkFormular() {
+	if (document.Formular.Name.value == "") {
+		alert("Bitte geben Sie ihr Nutzerkennung ein!");
+		}
+
+    if (document.Formular.Verbrauch.value == "") {
+		alert("Bitte Ihren Verbrauchswert (in kWh) ein!");
+	}
+	        
+    }
+
+    
+
+
+
