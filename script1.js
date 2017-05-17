42a851ce6d509a3217ed34cc987169f86dc58b02
flattermann <friedrich.lattermann@kaeuferportal.de>
2017-05-17 11:08:15 +0200

    Quick refactoring to make the code more readable

--- script1.js
+++ script1.js
@@ -1,6 +1,6 @@
 $( document ).ready(function(){
 
- 
+
 });
 
 function zufallsZahl1() {
@@ -10,43 +10,37 @@ function zufallsZahl1() {
     var max = 240;
     var min2 = 0;
     var max2 = 7;
- 
-    
-    elem2 = (Math.random() * (max - min + 1)) + min;
-    elem1 = Runden2Dezimal(elem2)
 
-    if(elem1 != ""){
+
+    elem1 = (Math.random() * (max - min + 1)) + min;
+    elem2 = Runden2Dezimal(elem1)
+
+    if(elem2 != ""){
         elem4=(Math.random() * (max2 - min2 + 1)) + min2;
         elem3 = Runden2Dezimal(elem4)
         document.getElementById("ausgabe2").innerHTML = elem3;
+
         if(elem3 > 5){
         alert("Warnung, Stromstärke überprüfen!");
+        }
     }
-    }
-    
 
     document.getElementById("ausgabe1").innerHTML = elem1;
-    
 }
 
 
-function Runden2Dezimal(elem1) { 
-    Ergebnis = Math.round(elem1 * 10) / 10 ; 
-    return Ergebnis; }
-
-
-
+function Runden2Dezimal(elem1) {
+    Ergebnis = Math.round(elem1 * 10) / 10 ;
+    return Ergebnis;
+}
 
 var elem = document.getElementById('los');
 elem.addEventListener('click', chkFormular);
 
-
-if (document.Formular.Verbrauch.value != "" && document.Formular.Name != "") { 
+if (document.Formular.Verbrauch.value != "" && document.Formular.Name != "") {
         elemUhr = new Date();
-		//alert("Test!");
         document.getElementById("ausgabe3").innerHTML = new Date;
-    
-    }
+}
 
 function chkFormular() {
 	if (document.Formular.Name.value == "") {
@@ -56,10 +50,4 @@ function chkFormular() {
     if (document.Formular.Verbrauch.value == "") {
 		alert("Bitte Ihren Verbrauchswert (in kWh) ein!");
 	}
-	        
-    }
-
-    
-
-
-
+}
