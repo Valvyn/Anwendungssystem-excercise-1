48ffd944131150dab81c8d8bba0d2c0275e2dcd5
flattermann <friedrich.lattermann@kaeuferportal.de>
2017-05-17 11:27:16 +0200

    Fix indentation

--- script1.js
+++ script1.js
@@ -4,47 +4,47 @@ $( document ).ready(function(){
 });
 
 function zufallsZahl1() {
-    var min = 220;
-    var max = 240;
-    var minAmpere = 0;
-    var maxAmpere = 7;
+  var min = 220;
+  var max = 240;
+  var minAmpere = 0;
+  var maxAmpere = 7;
 
+  var randomVoltage = (Math.random() * (max - min + 1)) + min;
+  var roundedRandomVoltage = Runden2Dezimal(randomVoltage)
+
+  if(roundedRandomVoltage != ""){
     debugger
-    var randomVoltage = (Math.random() * (max - min + 1)) + min;
-    var roundedRandomVoltage = Runden2Dezimal(randomVoltage)
+    elem4=(Math.random() * (maxAmpere - minAmpere + 1)) + minAmpere;
+    elem3 = Runden2Dezimal(elem4)
+    document.getElementById("ausgabe2").innerHTML = elem3;
 
-    if(roundedRandomVoltage != ""){
-        elem4=(Math.random() * (maxAmpere - minAmpere + 1)) + minAmpere;
-        elem3 = Runden2Dezimal(elem4)
-        document.getElementById("ausgabe2").innerHTML = elem3;
-
-        if(elem3 > 5){
-        alert("Warnung, Stromstärke überprüfen!");
-        }
+    if(elem3 > 5){
+    alert("Warnung, Stromstärke überprüfen!");
     }
+  }
 
-    document.getElementById("ausgabe1").innerHTML = randomVoltage;
+  document.getElementById("ausgabe1").innerHTML = randomVoltage;
 }
 
 
 function Runden2Dezimal(randomVoltage) {
-    return Math.round(randomVoltage * 10) / 10 ;
+  return Math.round(randomVoltage * 10) / 10 ;
 }
 
 var elem = document.getElementById('los');
 elem.addEventListener('click', chkFormular);
 
 if (document.Formular.Verbrauch.value != "" && document.Formular.Name != "") {
-        elemUhr = new Date();
-        document.getElementById("ausgabe3").innerHTML = new Date;
+  elemUhr = new Date();
+  document.getElementById("ausgabe3").innerHTML = new Date;
 }
 
 function chkFormular() {
-	if (document.Formular.Name.value == "") {
-		alert("Bitte geben Sie ihr Nutzerkennung ein!");
-		}
+  if (document.Formular.Name.value == "") {
+  	alert("Bitte geben Sie ihr Nutzerkennung ein!");
+  	}
 
     if (document.Formular.Verbrauch.value == "") {
-		alert("Bitte Ihren Verbrauchswert (in kWh) ein!");
-	}
+  	alert("Bitte Ihren Verbrauchswert (in kWh) ein!");
+  }
 }
