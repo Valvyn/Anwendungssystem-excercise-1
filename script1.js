0655c5ebed4ab96b36426f7618010479b93156f8
Unknown <friedrichlattermann@hotmail.com>
2017-05-24 13:18:49 +0200

    Add  comments and format the code

--- script1.js
+++ script1.js
@@ -1,40 +1,48 @@
 $( document ).ready(function(){
-
-
 });
 
 function zufallsZahl1() {
+  //set the Voltages to work with
   var minVoltage = 220;
   var maxVoltage = 240;
   var minAmpere = 0;
 
+  //retrieve the maximum load from the current meter
   var maxLoad = document.getElementById("maxLoad").textContent;
   maxLoad = parseInt(maxLoad.match(/\d+/)[0]);
 
+  //create a random voltage and round to two decimals
   var randomVoltage = (Math.random() * (maxVoltage - minVoltage + 1)) + minVoltage;
   var roundedRandomVoltage = Runden2Dezimal(randomVoltage)
 
+
   if(roundedRandomVoltage != ""){
+    //create a random ampere value for each meter. Increment it by 5 and display it
     randomAmpere=((Math.random() * (maxLoad - minAmpere + 1)) + minAmpere) + 5;
     roundedRandomAmpere = Runden2Dezimal(randomAmpere)
     document.getElementById("ausgabe2").innerHTML = roundedRandomAmpere;
 
+    //display the voltage
+    document.getElementById("ausgabe1").innerHTML = roundedRandomVoltage;
+
+    //throw a warning if the current load exceeds the maximum ampere
     if(roundedRandomAmpere > maxLoad){
     alert("Warnung, Stromzähler überlastet!");
     }
   }
-
-  document.getElementById("ausgabe1").innerHTML = roundedRandomVoltage;
 }
 
-
+// function to round any float value to two decimals
 function Runden2Dezimal(randomVoltage) {
   return Math.round(randomVoltage * 10) / 10 ;
 }
 
+//start listening for clicks on the submit button on the form
 var elem = document.getElementById('los');
 elem.addEventListener('click', chkFormular);
 
+//this prevents the default operation for a form submit which is a post and a reload
+// we need to prohibit this because we have no data persistence through a backend
 var form = document.getElementById("submitForm");
 function handleForm(event) { event.preventDefault(); }
 form.addEventListener('submit', handleForm);
